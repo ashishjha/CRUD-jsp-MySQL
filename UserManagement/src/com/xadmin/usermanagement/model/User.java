d302c859a23eb33fcd96451090272da2a7eda10a
ASHISH JHA <55405046+ashishjha@users.noreply.github.com>
2021-09-02 12:36:54 +0530

    Added Files

--- UserManagement/src/com/xadmin/usermanagement/model/User.java
+++ UserManagement/src/com/xadmin/usermanagement/model/User.java
@@ -1,11 +1,6 @@
 package com.xadmin.usermanagement.model;
 
-/**
- * User.java
- * This is a model class represents a User entity
- * @author Ramesh Fadatare
- *
- */
+
 public class User {
 	protected int id;
 	protected String name;
